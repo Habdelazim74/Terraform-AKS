87dffec85d6144da6b58c75d8b3c027d61d7003c
Habdelazim74 <hossamserry74@gmail.com>
2025-04-17 13:09:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,4 +8,22 @@ resource "azurerm_resource_group" "rg1" {
   name     = var.rgname
   location = var.location
 }
+module "ServicePrincipal" {
+  source                 = "./modules/ServicePrincipal"
+  service_principal_name = var.service_principal_name
 
+  depends_on = [
+    azurerm_resource_group.rg1
+  ]
+}
+
+resource "azurerm_role_assignment" "rolespn" {
+
+  scope                = "/subscriptions/5f5470df-f806-47ee-8f78-6520f817df59"
+  role_definition_name = "Contributor"
+  principal_id         = module.ServicePrincipal.service_principal_object_id
+
+  depends_on = [
+    module.ServicePrincipal
+  ]
+}
